f53e962f988ad64476443ab310bc2dd91425fbeb
Yuriy Gerton <gerton88@gmail.com>
2018-05-18 01:34:27 +0300

    Add CreateUser and Login forms. Add fetch create user example.

---
+++ src/components/CreateUserForm.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  button: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
+
+class CreateUserForm extends React.Component {
+  state = {
+    username: {
+      value: '',
+      isValid: true,
+    },
+    password: {
+      value: '',
+      isValid: true,
+    },
+    repeatedPassword: {
+      value: '',
+      isValid: true,
+    },
+  };
+
+  validate = () => {
+    const { password, repeatedPassword } = this.state;
+    const isValid = password.value === repeatedPassword.value;
+
+    this.setState({
+      password: { ...password, isValid },
+      repeatedPassword: { ...repeatedPassword, isValid },
+    });
+
+    return isValid;
+  };
+  handleChangeInput = event => {
+    event.persist();
+    const { name, value } = event.target;
+    this.setState(prevState => ({
+      [name]: {
+        ...prevState[name],
+        value,
+      },
+    }));
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    if (!this.validate()) {
+      return;
+    }
+    const { username, password, repeatedPassword } = this.state;
+    console.log(
+      'Create Login: ',
+      username.value,
+      password.value,
+      repeatedPassword.value,
+    );
+    fetch('http://localhost:8000/v1/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(reason => console.error(reason));
+  };
+  render() {
+    const { classes } = this.props;
+    const { username, password, repeatedPassword } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <TextField
+          required
+          label="Username"
+          placeholder="Type your username..."
+          name="username"
+          type="text"
+          value={username.value}
+          onChange={this.handleChangeInput}
+          error={!username.isValid}
+          margin="normal"
+          autoComplete="username"
+          fullWidth
+        />
+        <TextField
+          required
+          label="Password"
+          placeholder="Type your password..."
+          name="password"
+          type="password"
+          value={password.value}
+          onChange={this.handleChangeInput}
+          error={!password.isValid}
+          margin="normal"
+          autoComplete="new-password"
+          fullWidth
+        />
+        <TextField
+          required
+          label="Repeat password"
+          placeholder="Repeat your password..."
+          name="repeatedPassword"
+          type="password"
+          value={repeatedPassword.value}
+          onChange={this.handleChangeInput}
+          error={!repeatedPassword.isValid}
+          margin="normal"
+          autoComplete="new-password"
+          fullWidth
+        />
+        <Button
+          variant="raised"
+          type="submit"
+          color="primary"
+          className={classes.button}
+          fullWidth
+        >
+          Signup
+        </Button>
+      </form>
+    );
+  }
+}
+export default withStyles(styles)(CreateUserForm);
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import WelcomePageContent from './WelcomePageContent';
 
-const WelcomePage = () => <h1>Welcome</h1>;
+const styles = theme => ({
+  welcomePageWrap: {
+    display: 'flex',
+    flexWrap: `wrap`,
+    justifyContent: `center`,
+    backgroundColor: theme.palette.background.default,
+    height: `100vh`,
+    width: `100%`,
+  },
+  welcomePage: {
+    width: `100%`,
+  },
+});
 
-export default WelcomePage;
+const WelcomePage = ({ classes }) => (
+  <div className={classes.welcomePageWrap}>
+    <div className={classes.welcomePage}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" noWrap>
+            DogeCodes React Chat
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <WelcomePageContent />
+    </div>
+  </div>
+);
+
+export default withStyles(styles)(WelcomePage);
