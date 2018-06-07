8f869d0f4c61a95f7b73305aab4c57609ef53d7e
Yuriy Gerton <gerton88@gmail.com>
2018-06-08 00:07:07 +0300

    Fix state. Add sockets.

--- src/actions/chats.js
+++ src/actions/chats.js
@@ -105,7 +105,7 @@ export function joinChat(chatId) {
           payload: { chat },
         });
         dispatch(redirect(`/chat/${chat._id}`));
-        dispatch(fetchChat(chatId));
+        dispatch(fetchChat(chatId)); // For showing join message
         return chat;
       })
       .catch(reason =>
--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import PrivateRoute from '../containers/PrivateRoute';
 import WelcomePage from '../containers/WelcomePage';
 import ChatPage from '../containers/ChatPage';
 import history from '../utils/history';
-import configureStore from '../store';
-
-const store = configureStore();
 
 const App = () => (
-  <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/(welcome)?" component={WelcomePage} />
-        <PrivateRoute path="/chat/:chatId?" component={ChatPage} />
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  </Provider>
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/(welcome)?" component={WelcomePage} />
+      <PrivateRoute path="/chat/:chatId?" component={ChatPage} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
 );
 
 export default App;
